10b67fa39a9a9b121ee3e49880311722c89917aa
Avocado-Programmer <97059134+StillThinkingHere@users.noreply.github.com>
2024-03-01 14:02:10 +0100

    Update WordChecker.py
    Added Comments,
    Made Letters lowercase for checks

--- WordChecker.py
+++ WordChecker.py
@@ -1,12 +1,18 @@
 from english_words import get_english_words_set
 from rich import print
-words, output = get_english_words_set(['web2'], lower=True) , []
-for word in list(str(input("Please type in a sentence and press enter to check: ")).split(" ")):
-    if word in words:
-        output.append(f"[green]{word}[/green]")
-    else:
-        output.append(f"[red]{word}[/red]")
-final_output = f"{output[0]}"
-for out in range(1, len(output)):
-    final_output = f"{final_output} {output[out]}"
-print(final_output)
+words = get_english_words_set(['web2'], lower=True)
+output = []
+
+while 1:
+    # Checks if words are correct
+    for word in list(str(input("Please type in a sentence and press enter to check: ")).split(" ")):
+        if word.lower() in words:
+            output.append(f"[green]{word}[/green]")
+        else:
+            output.append(f"[red]{word}[/red]")
+
+    # Turns List to string
+    final_output = f"{output[0]}"
+    for iteration in range(1, len(output)):
+        final_output = f"{final_output} {output[iteration]}"
+    print(final_output)
